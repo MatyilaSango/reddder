28191814d111fd93312feff5bc1b244460b87cbc
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-11-13 19:09:15 +0200

    added disclaimer

--- Components/Footer.js
+++ Components/Footer.js
@@ -16,7 +16,8 @@ export default function Footer() {
     <div className={styles.footerContainer}>
         <Link href="https://www.freeprivacypolicy.com/live/4d169eb0-07ab-4566-b202-5e926814f007" className={styles.ptLink}>Privacy Policy</Link> 
         <Link href="https://www.freeprivacypolicy.com/live/ee19918e-249d-4efd-be09-aed0d5fe4933" className={styles.ptLink}>Terms and Conditions</Link>
-        <p onClick={toContact} className={styles.ptLink}>Contact us</p>
+        <p onClick={toContact} className={styles.ptLink}>Contact us</p><br />
+        <p className={styles.disclaimer}>Reddder does not store any files on our server, we only linked to the media which is hosted on Reddit.</p>
       </div>
   )
 }
