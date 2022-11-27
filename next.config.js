c430e19b399b038856fe7c270649709d19a54a06
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-11-27 21:56:07 +0200

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
+
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development'
+//   },
+// }); 
+
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-module.exports = {
-  experimental: {
-    scrollRestoration: true,
+  swcMinify: true,
+  compiler: {
+    //removeConsole: process.env.NODE_ENV !== "development",
   },
 };
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  //disable: process.env.NODE_ENV === "development",
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
