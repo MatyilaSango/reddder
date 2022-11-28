24b17044aa8f82c3919da2a6755e96450ab0f011
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-11-28 16:42:38 +0200

    added amimation in load more button and in search bar

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -2,16 +2,20 @@ import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import redder from "../images/Reddder.png";
 import search from "../images/Search.png";
-import { React } from "react";
+import { React, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function NarvBar() {
   const router = useRouter();
+  const pleaseWaitRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false)
 
   const searchHandler = async (e) => {
     e.preventDefault();
     if (!e.currentTarget.elements[0].name) return;
     const param = e.currentTarget.elements[0].value;
+
+    setIsLoading(true)
 
     await router.push(`/Search/?q=${param}`);
     router.reload();
@@ -20,6 +24,10 @@ export default function NarvBar() {
   const toHome = () => {
     router.push("/");
   };
+
+  useEffect(() => {
+    pleaseWaitRef.current = "Please wait...";
+  }, [isLoading])
 
   return (
     <div className={styles.narvBar}>
@@ -43,6 +51,9 @@ export default function NarvBar() {
             <Image src={search} alt="search" width={20} />
           </button>
         </form>
+        <label className={styles.pleaseWaitLableText}>
+          {pleaseWaitRef.current}
+        </label>
         <div className={styles.searchItemsContainer}>
           <div className={styles.searchItem}>abcdefg</div>
         </div>
